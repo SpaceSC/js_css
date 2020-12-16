c9b2ba986e12a81f580a8e1deb270d638d4b4710
SpaceSC <ceciliasandor.22@gmail.com>
2020-12-16 10:19:04 +0100

    Tag and content to html with for

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 function pageLoad () {
 
 //tömb, belül egy objektum (tag: h1, content: Cecilia, )
-{
-  let myObject = {
+let myObjects = [
+    {
     tag:"h1", 
     content:"Cecilia"
-    
-    };
+    },
+    {
+    tag:"p", 
+    content:"Lorem ipsum"
+    }
+  ];
+
+  /*
+  for (let index = 0; index < myObjects.length; index++) {   
+  }
+  */
+let myRoot = document.getElementById("root");
+
+// array mindig tartja a sorrendet, object nem sorrendben logol ki
+ for(myObject of myObjects){
+   // console.log(myObject.content);
+   myRoot.insertAdjacentHTML("beforeend", `
+   <${myObject.tag}>${myObject.content}</${myObject.tag}>
+   `
+   );
+ }
 
 }
 
-
-}
-
-window.addEventListener("load", pageLoad); //ahol event van, ott van ()
+window.addEventListener("load", pageLoad); // ahol event van, ott van ()
